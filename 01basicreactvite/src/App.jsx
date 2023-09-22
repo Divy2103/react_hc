e80bb456a0eb9466e52eb351aae2e0b7fca8282e
divykaila <kailadivy098@gmail.com>
2023-09-22 22:06:47 +0530

    create custom react library and jsx

--- 01basicreactvite/src/App.jsx
+++ 01basicreactvite/src/App.jsx
@@ -1,11 +1,15 @@
 import Youtube from './Youtube'
 
-function App() {
 
+const userName = 'divy'
+function App() {
   return (
     <>
       <h1>React app with vite</h1>
-      <Youtube/>
+      <h1>vite react app {2 + 2}</h1>
+      <h1>my name is {userName}</h1>
+      <h1>my name is {}</h1>
+      <Youtube />
     </>
   )
 }
--- 01basicreactvite/src/main.jsx
+++ 01basicreactvite/src/main.jsx
@@ -2,8 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  children: 'click me to visit google'
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>custom react app</h1>
+    </div>
+  )
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const AreactElement = React.createElement(
+  'a',  // element
+  { href: 'https://google.com', target: '_blank' }, // attributes
+
+  /*
+  userName, 
+  2 + 2  // working
+  // if(userName === 'divy') ? 'yes' : 'no',   // not working
+  */
+
+  'click me to visit google', // children
+  " ",
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
+  // <MyApp />
+  // AnotherElement
 )
