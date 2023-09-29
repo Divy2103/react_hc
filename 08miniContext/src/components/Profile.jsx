3277a2d7fad30cff45e5f757142a0450c8ceead4
divykaila <kailadivy098@gmail.com>
2023-09-29 10:26:47 +0530

    context api discussion and create one project - how to send and recive data

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React, { isValidElement, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+    const { user } = useContext(UserContext);
+
+    if (!user) return <div>Please Login</div>
+
+    return <div>Welcome {user.username} your password is {user.password}</div>
+}
+
+export default Profile
